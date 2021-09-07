8d4adda35d09b87ebd623778235301c5f7c6080a
BriaN890501 <45000802+BriaN890501@users.noreply.github.com>
2021-09-07 09:57:11 +0800

    feat: add word cloud & chart components
    新增文字雲、圖表components，已確認互動功能皆可正常執行，待切版完成再進行微調

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+
+import editor from './editor';
 
 const store = configureStore({
-  reducer: {}, // a map of reducer, 之後在 useSelector 取用時需改為 state.{reducer name}
+  reducer: { editor }, // a map of reducer, 之後在 useSelector 取用時需改為 state.{reducer name}
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger), // apply middleware
 });
 
 export default store;
