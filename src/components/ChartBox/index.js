c5b74fdfe2692185f1e36075cde816dcafb0f1c8
raymanlee89 <raymanlee89@gmail.com>
2021-09-08 22:09:55 +0800

    Add stackedbarplot and wordcloud in chartbox.

--- src/components/ChartBox/index.js
+++ src/components/ChartBox/index.js
@@ -2,9 +2,12 @@ import classnames from 'classnames';
 import { useState, useEffect } from 'react';
 
 import { chartBox, mid, large } from './index.module.css';
+import WordCloud from '../Chart/WordCloud';
+import StackedBarFull from '../Chart/StackedBarFull';
 
 // size : mid / large
-const ChartBox = ({ className, size, chartTitle }) => {
+// type : WordCloud / StackedBarFull
+const ChartBox = ({ className, size, chartTitle, type }) => {
     const [sizeClass, setSizeClass] = useState(mid);
     useEffect(() => {
         switch (size) {
@@ -22,6 +25,16 @@ const ChartBox = ({ className, size, chartTitle }) => {
     return (
         <div className={classnames(chartBox, sizeClass, className)}>
             {chartTitle}
+            {(() => {
+                switch (type) {
+                    case 'WordCloud':
+                        return <WordCloud />
+                    case 'StackedBarFull':
+                        return <StackedBarFull />
+                    default:
+                        return <></>
+                }
+            })()}
         </div>
     );
 };
--- src/pages/Overview/index.js
+++ src/pages/Overview/index.js
@@ -4,10 +4,6 @@ import ChartBox from '../../components/ChartBox';
 
 import { container, overviewWrapper, chartBoxWrapper } from './index.module.css';
 
-//components can be added after layout completion
-//import WordCloud from '../../components/WordCloud';
-//import StackedBarFull from '../../components/StackedBarFull';
-
 const Overview = () => {
   return (
     <div className={container}>
@@ -15,11 +11,12 @@ const Overview = () => {
       <div className={overviewWrapper}>
         <Title>Overview</Title>
         <div className={chartBoxWrapper}>
-          <ChartBox size='large' chartTitle='熱門關鍵字' />
+          <ChartBox size='large' chartTitle='熱門關鍵字' type='WordCloud' />
+          <ChartBox size='mid' chartTitle='近期專案成功率' />
           <ChartBox size='mid' chartTitle='歷年專案成功率' />
           <ChartBox size='mid' chartTitle='歷年專案達成率' />
           <ChartBox size='mid' chartTitle='歷年募得金額' />
-          <ChartBox size='mid' chartTitle='歷年各類別比例' />
+          <ChartBox size='mid' chartTitle='歷年各類別比例' type='StackedBarFull' />
           <ChartBox size='mid' chartTitle='各類別成敗比例' />
         </div>
       </div>
