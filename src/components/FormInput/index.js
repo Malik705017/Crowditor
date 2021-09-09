b59d280a9e14207b65f91c3bb0317c9e399154c2
Malik705017 <fhh1482@gmail.com>
2021-09-09 13:07:42 +0800

    feat: add FormInput component and implement input examination

---
+++ src/components/FormInput/index.js
@@ -0,0 +1,25 @@
+import classNames from 'classnames';
+
+import { formInput, error } from './index.module.css';
+
+const FormInput = ({ onChange, onBlur, type, value, isValid, inputType, className }) => {
+  return (
+    <input
+      className={classNames(className, formInput, {
+        [error]: !isValid,
+      })}
+      type={inputType}
+      value={value}
+      onChange={e =>
+        onChange({
+          key: 'value',
+          value: e.target.value,
+          type,
+        })
+      }
+      onBlur={() => onBlur({ key: 'isValid', value, type, inputType })}
+    ></input>
+  );
+};
+
+export default FormInput;
