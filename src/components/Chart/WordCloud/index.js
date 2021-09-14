8d2fa13ff814f3dcdd0467d6b587fa41cd7267b3
BriaN890501 <45000802+BriaN890501@users.noreply.github.com>
2021-09-14 19:28:10 +0800

    feat: add mock response data to word cloud, alter word cloud  scale
    換分類的按鈕樣子要改 ( 或是看怎麼排版
    scale改成log看起來比較舒服

--- src/components/Chart/WordCloud/index.js
+++ src/components/Chart/WordCloud/index.js
@@ -4,7 +4,7 @@ import ReactWordcloud from "react-wordcloud";
 import "tippy.js/dist/tippy.css";
 import "tippy.js/animations/scale.css";
 
-import { series1, series2 } from "../../../resources/data/WordCloud";
+import { all, publish, local, comic, photo, education, fashion, society, tech, space, art, perform, design, game, movie, music, food } from "../../../resources/data/WordCloud";
 
 const options = {
   colors: ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b"],
@@ -17,28 +17,118 @@ const options = {
   padding: 1,
   rotations: 2, //restrict to vertical & horizontal
   rotationAngles: [0, 90],
-  scale: "sqrt",
+  scale: "log",
   spiral: "archimedean",
   transitionDuration: 1000 //animation
 };
 
 export default function WordCloud() {
-  const [graph , setGraph] = useState(<ReactWordcloud options={options} words={series1} />);
+  const [graph , setGraph] = useState(<ReactWordcloud options={options} words={all} />);
 
-  const changedata1 = () => {
-    setGraph(<ReactWordcloud options={options} words={series1} />)
+  const changeAll = () => {
+    setGraph(<ReactWordcloud options={options} words={all} />)
   }
-  const changedata2 = () => {
-    setGraph(<ReactWordcloud options={options} words={series2} />)
+  const changePublish = () => {
+    setGraph(<ReactWordcloud options={options} words={publish} />)
+  }
+  const changeLocal = () => {
+    setGraph(<ReactWordcloud options={options} words={local} />)
+  }
+  const changeComic = () => {
+    setGraph(<ReactWordcloud options={options} words={comic} />)
+  }
+  const changePhoto = () => {
+    setGraph(<ReactWordcloud options={options} words={photo} />)
+  }
+  const changeEducation = () => {
+    setGraph(<ReactWordcloud options={options} words={education} />)
+  }
+  const changeFashion = () => {
+    setGraph(<ReactWordcloud options={options} words={fashion} />)
+  }
+  const changeSociety = () => {
+    setGraph(<ReactWordcloud options={options} words={society} />)
+  }
+  const changeTech = () => {
+    setGraph(<ReactWordcloud options={options} words={tech} />)
+  }
+  const changeSpace = () => {
+    setGraph(<ReactWordcloud options={options} words={space} />)
+  }
+  const changeArt = () => {
+    setGraph(<ReactWordcloud options={options} words={art} />)
+  }
+  const changePerform = () => {
+    setGraph(<ReactWordcloud options={options} words={perform} />)
+  }
+  const changeDesign = () => {
+    setGraph(<ReactWordcloud options={options} words={design} />)
+  }
+  const changeGame = () => {
+    setGraph(<ReactWordcloud options={options} words={game} />)
+  }
+  const changeMovie = () => {
+    setGraph(<ReactWordcloud options={options} words={movie} />)
+  }
+  const changeMusic = () => {
+    setGraph(<ReactWordcloud options={options} words={music} />)
+  }
+  const changeFood = () => {
+    setGraph(<ReactWordcloud options={options} words={food} />)
   }
 
   return (
     <div>
-      <button onClick={ changedata1 }>
-         data1
+      <button onClick={ changeAll }>
+         全部類別
       </button>
-      <button onClick={ changedata2 }>
-         data2
+      <button onClick={ changePublish }>
+         出版
+      </button>
+      <button onClick={ changeLocal }>
+         地方創生
+      </button>
+      <button onClick={ changeComic }>
+         插畫漫畫
+      </button>
+      <button onClick={ changePhoto }>
+         攝影
+      </button>
+      <button onClick={ changeEducation }>
+         教育
+      </button>
+      <button onClick={ changeFashion }>
+         時尚
+      </button>
+      <button onClick={ changeSociety }>
+         社會
+      </button>
+      <button onClick={ changeTech }>
+         科技
+      </button>
+      <button onClick={ changeSpace }>
+         空間
+      </button>
+      <button onClick={ changeArt }>
+         藝術
+      </button>
+      <button onClick={ changePerform }>
+         表演
+      </button>
+      <button onClick={ changeDesign }>
+         設計
+      </button>
+      <button onClick={ changeGame }>
+         遊戲
+      </button>
+      <button onClick={ changeMovie }>
+         電影動畫
+      </button>
+      <button onClick={ changeMusic }>
+         音樂
+      </button>
+      <button onClick={ changeFood }>
+         飲食
       </button>
       { graph }
     </div>
