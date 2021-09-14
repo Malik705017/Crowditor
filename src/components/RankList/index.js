24890b302089273a8f7e59a7bd197d314a2ac1d7
raymanlee89 <raymanlee89@gmail.com>
2021-09-14 15:51:02 +0800

    fix: modify the RankList css

--- src/components/RankList/index.js
+++ src/components/RankList/index.js
@@ -1,20 +1,20 @@
-import { rankList, rankHeader, rankHeaderColumn, rankItem, rankItemColumn } from './index.module.css';
+import { rankList, rankHeader, rankHeaderCell, rankItem, rankItemCell } from './index.module.css';
 
 export default function RankList ({columns=[], items=[]}) {
     return (
         <table className={rankList}>
             <thead className={rankHeader}>
-                <th className={rankHeaderColumn}>名次</th>
+                <th className={rankHeaderCell}>名次</th>
                 {columns.map((column) => (
-                    <th className={rankHeaderColumn}>{column}</th>
+                    <th className={rankHeaderCell}>{column}</th>
                 ))}
             </thead>
             <tbody>
                 {items.map((item, i) => (
                     <tr className={rankItem}>
-                        <td className={rankItemColumn}>{i+1}</td>
+                        <td className={rankItemCell}>{i+1}</td>
                         {item.map((column) => (
-                            <td className={rankItemColumn}>{column}</td>
+                            <td className={rankItemCell}>{column}</td>
                         ))}
                     </tr>
                 ))}
