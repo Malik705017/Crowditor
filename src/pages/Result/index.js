9f5b6050f3a0790bbe6357be19943566a6440cf4
Malik705017 <fhh1482@gmail.com>
2021-09-26 13:45:35 +0800

    feat: add loading UI for result page

--- src/pages/Result/index.js
+++ src/pages/Result/index.js
@@ -1,12 +1,12 @@
-import classnames from "classnames";
+import classnames from 'classnames';
 
-import { useSelector } from "react-redux";
+import { useSelector } from 'react-redux';
 
-import ProjectSidebar from "../../components/ProjectSidebar";
-import Title from "../../components/Title";
-import StackedBar from "../../components/Chart/StackedBar";
-import RankList from "../../components/RankList";
-import Chartbox from "../../components/Chartbox";
+import ProjectSidebar from '../../components/ProjectSidebar';
+import Title from '../../components/Title';
+import StackedBar from '../../components/Chart/StackedBar';
+import RankList from '../../components/RankList';
+import Chartbox from '../../components/Chartbox';
 //import { eachRangeSuccessRate } from '../../resources/data/StackedBar';
 
 import {
@@ -20,8 +20,10 @@ import {
   chartBoxWrapper,
   mid,
   rankInfoWrapper,
-  continueButton
-} from "./index.module.css";
+  continueButton,
+  loading,
+  loader,
+} from './index.module.css';
 
 const ResultPage = () => {
   const resultState = useSelector(state => state.result);
@@ -31,71 +33,66 @@ const ResultPage = () => {
       <ProjectSidebar />
       <div className={resultWrapper}>
         <Title>Result</Title>
-        <div className={columnCenter}>
-          <div className={rowCenter}>
-            <div>
-              <h2>專案評分</h2>
-              <div className={classnames(score, rowBasline)}>
-                <h1>{resultState.score}</h1>
-                <h3>/100</h3>
-              </div>
-            </div>
-            <div className={textBox}>
-              <div className={rowBasline}>
-                <h2>您的分數超過</h2>
-                <h1> {resultState.greater_than}% </h1>
-                <h2>的專案</h2>
-              </div>
-              <hr/>
-              <div className={rowBasline}>
-                <h2>分數介於</h2>
-                <h1>
-                  {
-                    resultState.success_rates_by_score.xaxis.categories[
-                      resultState.score_rank_index
-                    ]
-                  }
-                </h1>
-                <h2>的專案中，有</h2>
-                <h1>
-                  {
-                    resultState.success_rates_by_score.series[0].data[
-                      resultState.score_rank_index
-                    ]
-                  }
-                </h1>
-                <h2>% 的專案都成功了</h2>
-              </div>
-            </div>
+        {resultState.score === 0 && resultState.score_rank_index === 0 ? (
+          <div className={loader}>
+            <h3>Please wait for analyzing</h3>
+            <div className={loading} />
           </div>
-          <div className={classnames(rankInfoWrapper, columnCenter)}>
-            <h2>各分數級距成功率</h2>
-            <div className={mid}>
-              <StackedBar data={resultState.success_rates_by_score} full />
-            </div>
-          </div>
-          {resultState.peer_cnt !== 0 &&
-            <>
-              <h1>相似專案列表</h1>
-              <RankList
-                title='相似專案列表'
-                columns={["名次", "專案名稱", "專案類別", "專案性質", "結果"]}
-                items={resultState.peers.items}
-                type='peers'
-              />
-              <h1>相似專案特性分布</h1>
-              <div className={chartBoxWrapper}>
-                <Chartbox title="目標金額" unit="NTD" data={resultState.goal}/>
-                <Chartbox title="募資時長" unit="天" data={resultState.duration_days}/>
-                <Chartbox title="簡介字數" unit="字" data={resultState.description_length}/>
-                <Chartbox title="內文字數" unit="字" data={resultState.content_length}/>
-                <Chartbox title="最高贊助方案金額" unit="NTD" data={resultState.max_set_prices}/>
-                <Chartbox title="最低贊助方案金額" unit="NTD" data={resultState.min_set_prices}/>
+        ) : (
+          <div className={columnCenter}>
+            <div className={rowCenter}>
+              <div>
+                <h2>專案評分</h2>
+                <div className={classnames(score, rowBasline)}>
+                  <h1>{resultState.score}</h1>
+                  <h3>/100</h3>
+                </div>
               </div>
-            </>
-          }
-          <button className={continueButton}>繼續編輯</button>
-        </div>
+              <div className={textBox}>
+                <div className={rowBasline}>
+                  <h2>您的分數超過</h2>
+                  <h1> {resultState.greater_than}% </h1>
+                  <h2>的專案</h2>
+                </div>
+                <hr />
+                <div className={rowBasline}>
+                  <h2>分數介於</h2>
+                  <h1>{resultState.success_rates_by_score.xaxis.categories[resultState.score_rank_index]}</h1>
+                  <h2>的專案中，有</h2>
+                  <h1>{resultState.success_rates_by_score.series[0].data[resultState.score_rank_index]}</h1>
+                  <h2>% 的專案都成功了</h2>
+                </div>
+              </div>
+            </div>
+            <div className={classnames(rankInfoWrapper, columnCenter)}>
+              <h2>各分數級距成功率</h2>
+              <div className={mid}>
+                <StackedBar data={resultState.success_rates_by_score} full />
+              </div>
+            </div>
+            {resultState.peer_cnt !== 0 && (
+              <>
+                <h1>相似專案列表</h1>
+                <RankList
+                  title='相似專案列表'
+                  columns={['名次', '專案名稱', '專案類別', '專案性質', '結果']}
+                  items={resultState.peers.items}
+                  type='peers'
+                />
+                <h1>相似專案特性分布</h1>
+                <div className={chartBoxWrapper}>
+                  <Chartbox title='目標金額' unit='NTD' data={resultState.goal} />
+                  <Chartbox title='募資時長' unit='天' data={resultState.duration_days} />
+                  <Chartbox title='簡介字數' unit='字' data={resultState.description_length} />
+                  <Chartbox title='內文字數' unit='字' data={resultState.content_length} />
+                  <Chartbox title='最高贊助方案金額' unit='NTD' data={resultState.max_set_prices} />
+                  <Chartbox title='最低贊助方案金額' unit='NTD' data={resultState.min_set_prices} />
+                </div>
+              </>
+            )}
+            <button className={continueButton}>繼續編輯</button>
+          </div>
+        )}
       </div>
     </div>
   );
